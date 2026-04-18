9073a846635e5ae277bf1a8c7b85a3dfa4d24b07
LaiVanChung <126126468+tothichcau02@users.noreply.github.com>
2026-04-18 09:46:35 +0700

    #10 -phan quyen truy cap vao admin

---
+++ src/pages/admin/AdminUsers/AdminUsers.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import NotFoundPage from "../../NotFoundPage/NotFoundPage";
+
+export default function AdminDashboard() {
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    const userRole = localStorage.getItem("userRole");
+    if (userRole === "admin") {
+      setIsAdmin(true);
+    }
+    setLoading(false);
+  }, []);
+  
+  if (loading) return <div>Loading...</div>;
+  
+  if (!isAdmin) {
+    return <NotFoundPage />;
+  }
+  
+  return <div>Admin Users</div>;
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,32 +3,44 @@ import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 import Register from "../pages/auth/RegisterPage/Register";
 import LoginPage from "../pages/auth/LoginPage/Login";
 import OtpVerification from "../pages/auth/OtpVerificationPage/OtpVerification";
+import AdminDashboard from "../pages/admin/AdminDashboard/AdminDashboard";
+import AdminUsers from "../pages/admin/AdminUsers/AdminUsers";
 const routes = [
-    {
-        path: "/",
-        page: HomePage,
-        isShowHeader: true
-    },
-    {
-        path: "*",
-        page: NotFoundPage,
-        isShowHeader: false
-    },
-    {
-        path: "/register",
-        page: Register,
-        isShowHeader: false
-    },
-    {
-        path: "/login",
-        page: LoginPage,
-        isShowHeader: false
-    },
-    {
-        path: "/verify-otp",
-        page: OtpVerification,
-        isShowHeader: false
-    }
-]
+  {
+    path: "/",
+    page: HomePage,
+    isShowHeader: true,
+  },
+  {
+    path: "*",
+    page: NotFoundPage,
+    isShowHeader: false,
+  },
+  {
+    path: "/register",
+    page: Register,
+    isShowHeader: false,
+  },
+  {
+    path: "/login",
+    page: LoginPage,
+    isShowHeader: false,
+  },
+  {
+    path: "/verify-otp",
+    page: OtpVerification,
+    isShowHeader: false,
+  },
+  {
+    path: "/admin/dashboard",
+    page: AdminDashboard,
+    isShowHeader: false,
+  },
+  {
+    path: "/admin/users",
+    page: AdminUsers,
+    isShowHeader: false,
+  },
+];
 
 export default routes;
